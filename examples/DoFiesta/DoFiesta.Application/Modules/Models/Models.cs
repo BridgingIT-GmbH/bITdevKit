2a7d0a510577dbc95ffdaca1e43dcac4dbafcc57
Vincent van Proosdij <164303+vip32@users.noreply.github.com>
2025-01-30 23:19:00 +0100

    enumerations api

--- examples/DoFiesta/DoFiesta.Application/Modules/Models/Models.cs
+++ examples/DoFiesta/DoFiesta.Application/Modules/Models/Models.cs
@@ -5,6 +5,8 @@
 
 namespace BridgingIT.DevKit.Examples.DoFiesta.Application.Modules.Core;
 
+using BridgingIT.DevKit.Examples.DoFiesta.Domain.Model;
+
 public class TodoItemModel
 {
     public string Id { get; set; }
@@ -62,4 +64,17 @@ public class SubscriptionModel
     public DateTimeOffset? EndDate { get; set; }
 
     public string ConcurrencyVersion { get; set; }
+}
+
+public class EnumerationModel
+{
+    public IEnumerable<TodoStatus> TodoStatuses { get; set; } = TodoStatus.GetAll();
+
+    public IEnumerable<TodoPriority> TodoPriorities { get; set; } = TodoPriority.GetAll();
+
+    public IEnumerable<SubscriptionStatus> SubscriptionStatuses { get; set; } = SubscriptionStatus.GetAll();
+
+    public IEnumerable<SubscriptionPlan> SubscriptionPlans { get; set; } = SubscriptionPlan.GetAll();
+
+    public IEnumerable<SubscriptionBillingCycle> SubscriptionBillingCycles { get; set; } = SubscriptionBillingCycle.GetAll();
 }
---
+++ examples/DoFiesta/DoFiesta.Presentation.Web.Server/Modules/Core/Controllers/EnumerationController.cs
@@ -0,0 +1,32 @@
+﻿// MIT-License
+// Copyright BridgingIT GmbH - All Rights Reserved
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
+
+namespace BridgingIT.DevKit.Examples.DoFiesta.Presentation.Web.Server.Modules.Core.Controllers;
+
+using System.Net;
+using Application.Modules.Core;
+using BridgingIT.DevKit.Application.Identity;
+using BridgingIT.DevKit.Examples.DoFiesta.Domain.Model;
+using BridgingIT.DevKit.Presentation;
+using Common;
+using DevKit.Presentation.Web;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+[Route("api/core/enumerations")]
+[ApiController]
+public class EnumerationController() : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    public ActionResult<EnumerationModel> Get()
+    {
+        return this.Ok(new EnumerationModel());
+    }
+}

f8ee364fc066fc3a72c651762858ac2e58849d94
Vincent van Proosdij <164303+vip32@users.noreply.github.com>
2025-01-30 22:53:06 +0100

    map

--- examples/DoFiesta/DoFiesta.Presentation.Web.Server/Modules/Core/CoreModuleMapperRegister.cs
+++ examples/DoFiesta/DoFiesta.Presentation.Web.Server/Modules/Core/CoreModuleMapperRegister.cs
@@ -13,24 +13,54 @@ public class CatalogMapperRegister : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        // Entity -> Model mappings
         config.ForType<TodoItem, TodoItemModel>()
-        .Map(dest => dest.Id, src => src.Id.Value.ToString())
-        .Map(dest => dest.Status, src => src.Status.Id)
-        .Map(dest => dest.Priority, src => src.Priority.Id)
-        .Map(dest => dest.Assignee, src => src.Assignee.Value)
-        .Map(dest => dest.ConcurrencyVersion, src => src.ConcurrencyVersion.ToString())
-        .IgnoreNullValues(true);
-
-        config.ForType<TodoStep, TodoStepModel>()
-        .Map(dest => dest.Id, src => src.Id.Value.ToString())
-        .Map(dest => dest.TodoItemId, src => src.TodoItemId.Value.ToString())
-        .Map(dest => dest.Status, src => src.Status.Id)
-        .IgnoreNullValues(true);
-
-        config.ForType<Subscription, SubscriptionModel>()
+            .Map(dest => dest.Id, src => src.Id.Value.ToString())
+            .Map(dest => dest.Status, src => src.Status.Id)
+            .Map(dest => dest.Priority, src => src.Priority.Id)
+            .Map(dest => dest.Assignee, src => src.Assignee.Value)
+            .Map(dest => dest.ConcurrencyVersion, src => src.ConcurrencyVersion.ToString())
             .IgnoreNullValues(true);
-            //.Map(d => d.Plan, s => s.Plan.Value)
-            //.Map(d => d.Status, s => s.Status.Value)
-            //.Map(d => d.BillingCycle, s => s.BillingCycle.Value);
+
+        // Model -> Entity mappings
+        config.ForType<TodoItemModel, TodoItem>()
+            .Map(dest => dest.Id, src => TodoItemId.Create(src.Id))
+            .Map(dest => dest.Status, src => TodoStatus.GetAll<TodoStatus>().First(x => x.Id == src.Status))
+            .Map(dest => dest.Priority, src => TodoPriority.GetAll<TodoPriority>().First(x => x.Id == src.Priority))
+            .Map(dest => dest.Assignee, src => EmailAddress.Create(src.Assignee))
+            .Map(dest => dest.ConcurrencyVersion, src => Guid.Parse(src.ConcurrencyVersion))
+            .IgnoreNullValues(true);
+
+        // Entity -> Model mappings
+        config.ForType<TodoStep, TodoStepModel>()
+            .Map(dest => dest.Id, src => src.Id.Value.ToString())
+            .Map(dest => dest.TodoItemId, src => src.TodoItemId.Value.ToString())
+            .Map(dest => dest.Status, src => src.Status.Id)
+            .IgnoreNullValues(true);
+
+        // Model -> Entity mappings
+        config.ForType<TodoStepModel, TodoStep>()
+            .Map(dest => dest.Id, src => TodoStepId.Create(src.Id))
+            .Map(dest => dest.TodoItemId, src => TodoItemId.Create(src.TodoItemId))
+            .Map(dest => dest.Status, src => TodoStatus.GetAll<TodoStatus>().First(x => x.Id == src.Status))
+            .IgnoreNullValues(true);
+
+        // Entity -> Model mappings
+        config.ForType<Subscription, SubscriptionModel>()
+            .Map(dest => dest.Id, src => src.Id.Value.ToString())
+            .Map(dest => dest.Plan, src => src.Plan.Id)
+            .Map(dest => dest.Status, src => src.Status.Id)
+            .Map(dest => dest.BillingCycle, src => src.BillingCycle.Id)
+            .Map(dest => dest.ConcurrencyVersion, src => src.ConcurrencyVersion.ToString())
+            .IgnoreNullValues(true);
+
+        // Model -> Entity mappings
+        config.ForType<SubscriptionModel, Subscription>()
+            .Map(dest => dest.Id, src => SubscriptionId.Create(src.Id))
+            .Map(dest => dest.Plan, src => SubscriptionPlan.GetAll<SubscriptionPlan>().First(x => x.Id == src.Plan))
+            .Map(dest => dest.Status, src => SubscriptionStatus.GetAll<SubscriptionStatus>().First(x => x.Id == src.Status))
+            .Map(dest => dest.BillingCycle, src => SubscriptionBillingCycle.GetAll<SubscriptionBillingCycle>().First(x => x.Id == src.BillingCycle))
+            .Map(dest => dest.ConcurrencyVersion, src => Guid.Parse(src.ConcurrencyVersion))
+            .IgnoreNullValues(true);
     }
 }

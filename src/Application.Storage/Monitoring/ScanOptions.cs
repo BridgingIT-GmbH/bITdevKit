792839d3e8081aba3c5d6c3be37ea05dbd48b1e4
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2025-03-20 21:59:12 +0100

    fm scanoptions

---
+++ src/Application.Storage/Monitoring/ScanOptions.cs
@@ -0,0 +1,28 @@
+﻿// MIT-License
+// Copyright BridgingIT GmbH - All Rights Reserved
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
+
+namespace BridgingIT.DevKit.Application.Storage.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Represents options for scanning, including processing behavior and timing settings.
+/// </summary>
+/// <param name="waitForProcessing">Indicates whether to wait for processing to complete before proceeding.</param>
+/// <param name="timeout">Specifies the maximum duration to wait for the operation to finish.</param>
+/// <param name="delayPerFile">Sets the time to pause between processing each file.</param>
+public class ScanOptions(bool waitForProcessing = false, TimeSpan? timeout = null, TimeSpan? delayPerFile = null)
+{
+    public bool WaitForProcessing { get; set; } = waitForProcessing;
+
+    public TimeSpan Timeout { get; set; } = timeout ?? new TimeSpan(0, 5, 0);
+
+    public TimeSpan DelayPerFile { get; set; } = delayPerFile ?? TimeSpan.Zero;
+
+    public static ScanOptions Default => new();
+}

27c8ca5467f06eb575797c23a45176e2830b04e1
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2025-04-15 14:30:13 +0200

    log anpassungen

--- src/Application.Storage/Monitoring/Handlers/LocationHandlerBase.cs
+++ src/Application.Storage/Monitoring/Handlers/LocationHandlerBase.cs
@@ -403,16 +403,16 @@ public abstract class LocationHandlerBase : ILocationHandler
     private class TypedLogger(ILogger logger)
     {
         public void LogInformationStartingHandler(string storageProviderLocationName, string storageProviderDescription) =>
-            logger.LogInformation("{LogKey} filemonitoring: starting handler (locationName={StorageProviderLocationName}, description={StorageProviderDescription})", Constants.LogKey, storageProviderLocationName, storageProviderDescription);
+            logger.LogInformation("{LogKey} filemonitoring: starting location handler (locationName={StorageProviderLocationName}, description={StorageProviderDescription})", Constants.LogKey, storageProviderLocationName, storageProviderDescription);
 
         public void LogInformationRealTimeStarted(string storageProviderLocationName) =>
             logger.LogInformation("{LogKey} filemonitoring: real-time watching started (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);
 
         public void LogInformationStoppingHandler(string storageProviderLocationName) =>
-            logger.LogInformation("{LogKey} filemonitoring: stopping handler (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);
+            logger.LogInformation("{LogKey} filemonitoring: stopping location handler (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);
 
         public void LogInformationHandlerStopped(string storageProviderLocationName) =>
-            logger.LogInformation("{LogKey} filemonitoring: handler stopped (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);
+            logger.LogInformation("{LogKey} filemonitoring: location handler stopped (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);
 
         public void LogInformationScanningLocation(string storageProviderLocationName) =>
             logger.LogInformation("{LogKey} filemonitoring: scanning location (locationName={StorageProviderLocationName})", Constants.LogKey, storageProviderLocationName);

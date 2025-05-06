7c684c56c91e9ae30c1d14ec939ea079f14c4da5
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2025-05-06 13:53:53 +0200

    nuget update

---
+++ src/Common.Abstractions/Version.cs
@@ -0,0 +1,15 @@
+﻿// MIT-License
+// Copyright BridgingIT GmbH - All Rights Reserved
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
+
+namespace BridgingIT.DevKit.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Version
+{
+}

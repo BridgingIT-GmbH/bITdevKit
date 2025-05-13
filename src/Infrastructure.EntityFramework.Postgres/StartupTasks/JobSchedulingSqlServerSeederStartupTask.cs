d2bfcc61bb8982736684d3e4a6366bbf556244a0
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2025-05-13 20:53:10 +0200

    Rename SQL Server seeder to PostgreSQL seeder
    Refactor `JobSchedulingSqlServerSeederStartupTask` to
    `JobSchedulingPostgresSeederStartupTask` to support
    PostgreSQL. Introduced `IDatabaseReadyService` for
    database readiness checks in the constructor and
    `ExecuteAsync` method. Updated connection string
    builder to `SqlConnectionStringBuilder` and modified
    logging statements to reflect the new class name and
    database type.

--- src/Infrastructure.EntityFramework.Postgres/StartupTasks/JobSchedulingSqlServerSeederStartupTask.cs
+++ src/Infrastructure.EntityFramework.Postgres/StartupTasks/JobSchedulingSqlServerSeederStartupTask.cs
@@ -5,35 +5,39 @@
 
 namespace BridgingIT.DevKit.Infrastructure.EntityFramework;
 
+using BridgingIT.DevKit.Domain.Repositories;
 using Common;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Npgsql;
 
-public class JobSchedulingSqlServerSeederStartupTask
+public class JobSchedulingPostgresSeederStartupTask
     : IStartupTask, IRetryStartupTask, ITimeoutStartupTask
 {
     private const string LogKey = "UTL";
-    private readonly ILogger<JobSchedulingSqlServerSeederStartupTask> logger;
+    private readonly ILogger<JobSchedulingPostgresSeederStartupTask> logger;
     private readonly string connectionString;
     private readonly string tablePrefix;
+    private readonly IDatabaseReadyService databaseReadyService;
 
-    public JobSchedulingSqlServerSeederStartupTask(ILoggerFactory loggerFactory, IConfiguration configuration)
+    public JobSchedulingPostgresSeederStartupTask(ILoggerFactory loggerFactory, IConfiguration configuration, IDatabaseReadyService databaseReadyService = null)
         : this(loggerFactory,
             configuration.GetSection("JobScheduling:Quartz", false)["quartz.dataSource.default.connectionString"],
             configuration.GetSection("JobScheduling:Quartz", false)["quartz.jobStore.tablePrefix"])
-    { }
+    {
+        this.databaseReadyService = databaseReadyService;
+    }
 
-    public JobSchedulingSqlServerSeederStartupTask(
+    public JobSchedulingPostgresSeederStartupTask(
         ILoggerFactory loggerFactory,
         string connectionString,
         string tablePrefix = "[public].[QRTZ_")
     {
         EnsureArg.IsNotNullOrEmpty(connectionString, nameof(connectionString));
 
-        this.logger = loggerFactory?.CreateLogger<JobSchedulingSqlServerSeederStartupTask>() ??
-            NullLoggerFactory.Instance.CreateLogger<JobSchedulingSqlServerSeederStartupTask>();
+        this.logger = loggerFactory?.CreateLogger<JobSchedulingPostgresSeederStartupTask>() ??
+            NullLoggerFactory.Instance.CreateLogger<JobSchedulingPostgresSeederStartupTask>();
         this.connectionString = connectionString;
         this.tablePrefix = tablePrefix.EmptyToNull() ?? "[public].[QRTZ_";
     }
@@ -46,6 +50,11 @@ public class JobSchedulingSqlServerSeederStartupTask
     {
         try
         {
+            if (this.databaseReadyService != null)
+            {
+                await this.databaseReadyService.WaitForReadyAsync(cancellationToken: cancellationToken).AnyContext();
+            }
+
             var connectionStringBuilder = new NpgsqlConnectionStringBuilder(this.connectionString);
             var database = connectionStringBuilder.Database;
             var sql = SqlStatements.QuartzTables(database, this.tablePrefix);

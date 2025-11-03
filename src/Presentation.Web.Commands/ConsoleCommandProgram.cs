87a40c6fc7ab03f7ed2a66fb52fa73530d5a7f25
vp <vp>
2025-11-03 17:56:16 +0100

    Migrate to slnx
    Expanded `BridgingIT.DevKit.slnx` to include platform configurations for "Any CPU," "x64," and "x86." Introduced a new folder structure with categories such as `/assets/`, `/benchmarks/`, `/docs/`, `/examples/`, `/src/`, and `/tests/`. Added configuration and documentation files, benchmarking projects, detailed documentation, and example projects like `DinnerFiesta`, `DoFiesta`, `EventSourcingDemo`, and `WeatherForecast`. Organized source and test projects into layers for `Common`, `Application`, `Domain`, `Infrastructure`, and `Presentation`. This setup supports a large-scale application with multiple components and testing capabilities.

--- src/Presentation.Web.Commands/ConsoleCommandProgram.cs
+++ src/Presentation.Web.Commands/ConsoleCommandProgram.cs
@@ -1,26 +1,26 @@
-﻿// MIT-License
-// Copyright BridgingIT GmbH - All Rights Reserved
-// Use of this source code is governed by an MIT-style license that can be
-// found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
+﻿//// MIT-License
+//// Copyright BridgingIT GmbH - All Rights Reserved
+//// Use of this source code is governed by an MIT-style license that can be
+//// found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
 
-// Usage examples:
-// program echo Hello
-// program echo Hello --upper
-// program echo "Hello World" --repeat3 --color yellow
-// program echo greet --repeat2 --upper --color green
-// program help
-// program help echo
+//// Usage examples:
+//// program echo Hello
+//// program echo Hello --upper
+//// program echo "Hello World" --repeat3 --color yellow
+//// program echo greet --repeat2 --upper --color green
+//// program help
+//// program help echo
 
-var builder = Host.CreateApplicationBuilder(args);
+//var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddConsoleCommands(cfg =>
-{
-    cfg.AddCommand<EchoConsoleCommand>(); // register commands
-});
+//builder.Services.AddConsoleCommands(cfg =>
+//{
+//    cfg.AddCommand<EchoConsoleCommand>(); // register commands
+//});
 
-using var host = builder.Build();
+//using var host = builder.Build();
 
-return await ConsoleCommands.RunAsync(host.Services, args);
+//return await ConsoleCommands.RunAsync(host.Services, args);

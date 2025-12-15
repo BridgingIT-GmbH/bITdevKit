753896e4bd5f4f4b28c691a883395ef6e65d7d20
vp <vp>
2025-12-15 17:02:46 +0100

    bump

--- examples/DoFiesta/DoFiesta.Presentation.Web.Server/Program.cs
+++ examples/DoFiesta/DoFiesta.Presentation.Web.Server/Program.cs
@@ -30,8 +30,6 @@ builder.Host.ConfigureAppConfiguration();
 builder.Services.AddModules(builder.Configuration, builder.Environment)
     .WithModule<CoreModule>();
 
-b
-
 // ===============================================================================================
 // Configure the services
 builder.Services.AddRequester()

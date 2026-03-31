5aab5471433e915a593f2f9431e0726a27430ded
vp <vp>
2026-03-31 18:12:08 +0200

    refactor: Update namespace for FluentValidatorExtensionsTests for improved organization

--- tests/Common.UnitTests/Utilities/Validation/FluentValidatorExtensionsTests.cs
+++ tests/Common.UnitTests/Utilities/Validation/FluentValidatorExtensionsTests.cs
@@ -3,7 +3,7 @@
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
 
-namespace BridgingIT.DevKit.Common.UnitTests.Utilities.Validation;
+namespace BridgingIT.DevKit.Common.UnitTests.Utilities;
 
 using FluentValidation;
 

a6f5609de4766423f591a2d5019684f5f5f1ee87
vp <vp>
2026-04-09 01:18:20 +0200

    Update OpenApi files to use Presentation.Web namespace
    Changed namespace from BridgingIT.DevKit.Presentation.Web.OpenApi to BridgingIT.DevKit.Presentation.Web in OpenApiSecurityDocumentTransformer.cs and OpenApiSecurityOptions.cs for consistency. No other code changes were made.

--- src/Presentation.Web/OpenApi/OpenApiSecurityDocumentTransformer.cs
+++ src/Presentation.Web/OpenApi/OpenApiSecurityDocumentTransformer.cs
@@ -3,7 +3,7 @@
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
 
-namespace BridgingIT.DevKit.Presentation.Web.OpenApi;
+namespace BridgingIT.DevKit.Presentation.Web;
 
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.Extensions.DependencyInjection;
